4011480d87400bc7e89e4c7fb981d443ebac865d
muraoka0803 <nayukimur@gmail.com>
2022-04-06 22:03:24 +0900

    都道府県一覧の表示テスト

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import './App.css';
 /* RESAS APIから情報を取得 */
 const apiKey = "KMVXho2JeqwEGj7mmawLCYh20DnKDYlzhyeAqnsK"; // APIキー
 
-// 都道府県一覧の取得
-
 class App extends Component{
+
+  constructor(){
+    super();
+    this.state = {
+      prefectures: {},
+    };
+  }
 
   componentDidMount(){
     // 都道府県の一覧を取得
@@ -14,12 +19,22 @@ class App extends Component{
     .then(response => response.json())
     .then(res => {
       console.log(res.result);
+      this.setState({prefectures: res.result});
     });
   }
 
+  // 都道府県一覧の表示
+  displayPref(props){
+    return(
+      <div key={props.prefCode}>{props.prefName}</div>
+    );
+  }
+
   render(){
+    const prefs = this.state.prefectures;
     return (
       <div className="App">
+        {Object.keys(prefs).map(i => this.displayPref(prefs[i]))}
         <div>Title</div>
         <div>チェックボックス</div>
         <div>グラフ</div>
